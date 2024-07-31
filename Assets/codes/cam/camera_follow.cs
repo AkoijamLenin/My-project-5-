88047aa0c71748d73f25b1fb7f8b64cf1d67bc59
AkoijamLenin <154910664+AkoijamLenin@users.noreply.github.com>
2024-07-31 21:08:27 +0530

    final webgl included

--- Assets/codes/cam/camera_follow.cs
+++ Assets/codes/cam/camera_follow.cs
@@ -6,10 +6,14 @@ public class camera_follow : MonoBehaviour
 {
     public Transform cam;
     public float caMPOS=1.4F;
+    public float caMPOSz;
+
+
 
     
     void Update()
     {
-        cam.transform.position = new Vector3(transform.position.x,transform.position.y+caMPOS,transform.position.z-6);
+        cam.transform.position = new Vector3(transform.position.x,transform.position.y+caMPOS,transform.position.z-caMPOSz);
     }
+    //6 is the default
 }
